bed277eecd0af052d0c291ef003915350534114b
Ryan Seit <ryan@ryanseit.com>
2020-06-11 19:36:40 -0400

    Added price to BuildControls.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -68,6 +68,7 @@ class BurgerBuilder extends Component {
 					addIngredient={this.addIngredientHandler}
 					removeIngredient={this.removeIngredientHandler}
 					disabled={disabledInfo}
+					price={this.state.totalPrice}
 				/>
 			</Aux>
 		);
