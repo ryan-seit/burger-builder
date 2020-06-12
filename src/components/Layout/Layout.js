a9c96040eda054fa29b2da3555d5bc725ee4fc9f
Ryan Seit <ryan@ryanseit.com>
2020-06-12 10:54:08 -0400

    Added base sidedrawer components to navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = props => (
 	// Wrap elements in the higher order component 'Aux'
 	<Aux>
 		<Toolbar />
+		<SideDrawer />
 		<main className={classes.Content}>{props.children}</main>
 	</Aux>
 );
