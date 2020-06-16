d1ffd548145fa08474ffb721782f55d7ce46ee74
Ryan Seit <ryan@ryanseit.com>
2020-06-16 16:56:16 -0400

    Updated state in ContactData to integrate dynamically with form inputs. Orders are now persisted with the right data to the server.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,56 @@ import Input from "../../../components/UI/Input/Input";
 
 class ContactData extends React.Component {
 	state = {
-		name: "",
-		email: "",
-		address: {
-			street: "",
-			postalCode: "",
+		orderForm: {
+			name: {
+				elementType: "input",
+				elementConfig: {
+					type: "text",
+					placeholder: "Your Name",
+				},
+				value: "",
+			},
+			street: {
+				elementType: "input",
+				elementConfig: {
+					type: "text",
+					placeholder: "Street",
+				},
+				value: "",
+			},
+			zipCode: {
+				elementType: "input",
+				elementConfig: {
+					type: "text",
+					placeholder: "ZIP Code",
+				},
+				value: "",
+			},
+			country: {
+				elementType: "input",
+				elementConfig: {
+					type: "text",
+					placeholder: "Country",
+				},
+				value: "",
+			},
+			email: {
+				elementType: "input",
+				elementConfig: {
+					type: "email",
+					placeholder: "Your Email",
+				},
+				value: "",
+			},
+			deliveryMethod: {
+				elementType: "select",
+				elementConfig: {
+					options: [
+						{ value: "fastest", displayValue: "Fastest" },
+						{ value: "cheapest", displayValue: "Cheapest" },
+					],
+				},
+			},
 		},
 		loading: false,
 	};
@@ -20,20 +65,17 @@ class ContactData extends React.Component {
 	orderHandler = event => {
 		event.preventDefault();
 		this.setState({ loading: true });
+		const formData = {};
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[
+				formElementIdentifier
+			].value;
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			// in a production environment, calculate the final price on the server to ensure code isn't manipulated during POST
 			price: this.props.price,
-			customer: {
-				name: "Ryan Seit",
-				address: {
-					street: "81 Prospect Street",
-					zipCode: "11211",
-					country: "USA",
-				},
-				email: "test@test.com",
-			},
-			deliveryMethod: "fastest",
+			orderData: formData,
 		};
 		// post to Firebase using the node name appended with '.json' for compatibility, second argument contains order data to be sent
 		axios
@@ -47,36 +89,40 @@ class ContactData extends React.Component {
 			});
 	};
 
+	inputChangedHandler = (event, inputIdentifier) => {
+		event.preventDefault();
+		const updatedOrderForm = {
+			...this.state.orderForm,
+		};
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier],
+		};
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		this.setState({ orderForm: updatedOrderForm });
+	};
+
 	render() {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key],
+			});
+		}
+
 		let form = (
-			<form>
-				<Input
-					inputtype='input'
-					type='text'
-					name='name'
-					placeholder='Your Name'
-				/>
-				<Input
-					inputtype='input'
-					type='text'
-					name='email'
-					placeholder='Your Email'
-				/>
-				<Input
-					inputtype='input'
-					type='text'
-					name='street'
-					placeholder='Street'
-				/>
-				<Input
-					inputtype='input'
-					type='text'
-					name='postal'
-					placeholder='Postal Code'
-				/>
-				<Button btnType='Success' clicked={this.orderHandler}>
-					PLACE ORDER
-				</Button>
+			<form onSubmit={this.orderHandler}>
+				{formElementsArray.map(formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						changed={event => this.inputChangedHandler(event, formElement.id)}
+					/>
+				))}
+				<Button btnType='Success'>PLACE ORDER</Button>
 			</form>
 		);
 		if (this.state.loading) {
